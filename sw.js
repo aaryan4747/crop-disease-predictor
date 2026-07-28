47ec0e623938ce4b558b9e71a265740d2e62a1f4
Aaryankarthik <aaryankarthik4710@gmail.com>
2026-07-28 19:14:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'crop-care-ai-v2';
+const CACHE_NAME = 'crop-care-ai-v3';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -12,9 +12,24 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
